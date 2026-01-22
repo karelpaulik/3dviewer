2818666e83d062c380f1c6bfb6af3813fa8c306a
karel.paulik <karelpaulik@email.cz>
2026-01-22 11:01:39 +0100

    added: vite.config.js (nyní možno přesunout dist (po npx vite build) do jiného projektu)

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Toto zajistí, že cesty v index.html budou začínat tečkou (./assets/...)
+  // a projekt bude fungovat v jakémkoli podadresáři
+  // Nutno, pokud chci dist použít v jiném projektu
+  base: './', 
+  
+  build: {
+    // Volitelné: pokud chceš mít vše v jednom souboru, 
+    // ale pro Three.js je lepší nechat výchozí nastavení
+  }
+})
