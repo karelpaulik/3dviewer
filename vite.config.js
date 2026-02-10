474135822b7d85fb8f907d119b088ca85887a1f0
karel.paulik <karelpaulik@email.cz>
2026-02-10 10:09:06 +0100

    dist -> docs

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ export default defineConfig({
   // Toto zajistí, že cesty v index.html budou začínat tečkou (./assets/...)
   // a projekt bude fungovat v jakémkoli podadresáři
   // Nutno, pokud chci dist použít v jiném projektu
-  base: './', 
+  // base: './', 
   
+  // build: {
+  //   // Volitelné: pokud chceš mít vše v jednom souboru, 
+  //   // ale pro Three.js je lepší nechat výchozí nastavení
+  // }
+
+  base: '/3dviewer/',
   build: {
-    // Volitelné: pokud chceš mít vše v jednom souboru, 
-    // ale pro Three.js je lepší nechat výchozí nastavení
+    outDir: 'docs'
   }
+
 })
