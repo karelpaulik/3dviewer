230896e150440dfb1454c0c334a193f9badb4eb5
karel.paulik <karelpaulik@email.cz>
2026-05-06 09:18:12 +0200

    added: file - Export self-contained HTML folder

--- src/main.js
+++ src/main.js
@@ -1364,10 +1364,12 @@ function addMainGui() {
     fileGui.add({ fn: exportAllModels }, 'fn').name('Export all to GLB');
     fileGui.add({ fn: exportAllModelsDraco }, 'fn').name('Export all to GLB (Draco)');
     fileGui.add({ fn: exportSelectedObject }, 'fn').name('Export selected to GLB');
-    fileGui.add({ fn() { exportToHTML(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML');
-    fileGui.add({ fn() { exportToHTMLDraco(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML (Draco)');
-    fileGui.add({ fn() { exportToHTMLObfuscated(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML obfuscated');
-    fileGui.add({ fn() { exportToHTMLObfuscatedDraco(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML obfuscated (Draco)');
+    const exportHtmlFolder = fileGui.addFolder('Export self-contained HTML');
+    exportHtmlFolder.close();
+    exportHtmlFolder.add({ fn() { exportToHTML(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML');
+    exportHtmlFolder.add({ fn() { exportToHTMLDraco(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML (Draco)');
+    exportHtmlFolder.add({ fn() { exportToHTMLObfuscated(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML obfuscated');
+    exportHtmlFolder.add({ fn() { exportToHTMLObfuscatedDraco(loadedModels, assemblyGui, viewProp, assemblyWriteToUserData, assemblyClearUserData); } }, 'fn').name('Export to HTML obfuscated (Draco)');
     registerGuiPanel('File', fileGui);
 
     // --- Edit panel ---
