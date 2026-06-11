a74bd4001cdde0c15766c5ae65211848854e6fd0
karel.paulik <karelpaulik@email.cz>
2026-06-11 10:38:58 +0200

    welcome dialog shortened

--- src/main.js
+++ src/main.js
@@ -7962,14 +7962,8 @@ function addHelpGui() {
     welcomeDialog.id = 'welcome-dialog';
     welcomeDialog.innerHTML = `
         <h2>BEDOBE</h2>
-        <h3>CAD Explorer</h3>
         <p><strong>Welcome to the 3D model exploration and documentation platform.</strong></p>
-        <p><strong>Created by International CAD developers community</strong></p>
-        <p>BEDOBE is a web-based CAD Explorer, assembly workflow editor, and document editor built with Three.js. It allows you to load various 3D models, explore their structure, create documentation, create step-by-step assembly instructions with smooth animations and much much more.</p>
         <p>The goal of the project is to provide a publicly available <strong>CAD explorer</strong> for everyone.</p>
-        <hr>
-        <p>Have an idea for improvement? Contact us at:</p>
-        <p><a href="mailto:info@bedobe.eu">info@bedobe.eu</a></p>
         <form method="dialog"><button>Start</button></form>
     `;
     welcomeDialog.addEventListener('click', e => { if (e.target === welcomeDialog) welcomeDialog.close(); });
